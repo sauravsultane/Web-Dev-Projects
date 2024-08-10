8a46844fb4bde588fdd08f6545036901dea66d59
sauravsultane <127301681+sauravsultane@users.noreply.github.com>
2024-08-10 23:23:06 +0530

    login page
    simple login page

---
+++ Login/app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.querySelector('.btn');
+    button.addEventListener('click', function() {
+        window.location.href = 'https://www.instagram.com/prashant_mogal_96k/';
+    });
+});
